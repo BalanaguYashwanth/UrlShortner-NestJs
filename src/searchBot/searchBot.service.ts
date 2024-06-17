45cd132bb07a6b9c2b2d5db9a55b6c2875707b89
Gaurav <hibyehello68@gmail.com>
2024-06-17 20:28:36 +0530

    made this using kendra-client

--- src/searchBot/searchBot.service.ts
+++ src/searchBot/searchBot.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import * as AWS from 'aws-sdk';
+import { KendraClient, QueryCommand } from '@aws-sdk/client-kendra';
 
 @Injectable()
 export class SearchBotService {
-  private readonly kendra: AWS.Kendra;
-  private readonly indexId: string = process.env.AWS_INDEX_ID; 
+  private readonly kendraClient: KendraClient;
+  private readonly indexId: string = process.env.AWS_INDEX_ID;
 
   constructor() {
-    AWS.config.update({ region: process.env.AWS_KENDRA_REGION,
+    this.kendraClient = new KendraClient({
+      region: process.env.AWS_KENDRA_REGION,
+      credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
-     }); 
-    this.kendra = new AWS.Kendra({ apiVersion: '2019-02-03' });
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
   }
 
   async searchBot(queryText: string): Promise<string[]> {
@@ -20,11 +22,13 @@ export class SearchBotService {
       QueryText: queryText,
     };
 
+    const command = new QueryCommand(params);
+
     try {
-      const data = await this.kendra.query(params).promise();
+      const data = await this.kendraClient.send(command);
       const textResults = data.ResultItems
-      .filter(result => result.Type === 'ANSWER' && result.Format === 'TEXT')
-      .map(result => result.DocumentExcerpt.Text);
+        .filter(result => result.Type === 'ANSWER' && result.Format === 'TEXT')
+        .map(result => result.DocumentExcerpt.Text);
       return textResults;
     } catch (error) {
       console.error('Error querying Kendra:', error);
